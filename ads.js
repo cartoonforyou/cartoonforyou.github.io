010a15ea88fec2483190f8e8b7aac947176d28c6
Aliomar2009512 <omarabulhummus2004@gmail.com>
2025-02-08 15:19:15 +0300

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let adScript = document.createElement("script");
+    adScript.src = "https://kulroakonsu.net/88/tag.min.js";
+    adScript.setAttribute("data-zone", "130229");
+    adScript.setAttribute("async", "true");
+    adScript.setAttribute("data-cfasync", "false");
+    document.head.appendChild(adScript);
+});
